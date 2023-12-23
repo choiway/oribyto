e834d8ec06785a17ec2c518a7868bc96f1f46f23
Wayne Choi <waynechoi@gmail.com>
2023-12-22 21:39:37 -0800

    Add ability to save and read settings
    Used golang Gob for this

--- cmd/init.go
+++ cmd/init.go
@@ -1,15 +1,16 @@
 /*
 Copyright © 2023 NAME HERE <EMAIL ADDRESS>
-
 */
 package cmd
 
 import (
 	"fmt"
-    "os"
-    // "database/sql"
-    _ "github.com/mattn/go-sqlite3"
+	"os"
+	// "database/sql"
+	_ "github.com/mattn/go-sqlite3"
 
+	"bufio"
+	"encoding/gob"
 	"github.com/spf13/cobra"
 )
 
@@ -25,7 +26,56 @@ This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("init called")
+		// output, err := exec.Command("gpg", "--list-secret-keys").Output()
+		//
+		// if err != nil {
+		//     fmt.Println("Error: ", err)
+		//     os.Exit(1)
+		// }
+		//
+		// fmt.Println(string(output))
+		settings := Settings{DefaultKeyEmail: "waynechoi@gmail.com"}
+        path := "settings.gob"
+		// write
+		WriteSettings(settings, path)
+
+		// read
+        s := ReadSettings(path)
+		fmt.Printf("%q", s)
 	},
+}
+
+type Settings struct {
+	DefaultKeyEmail string
+}
+
+// WriteSettings writes settings to a file
+func WriteSettings(settings Settings, filename string) {
+	out, err1 := os.Create(filename)
+	if err1 != nil {
+		fmt.Printf("File write error: %v\n", err1)
+		os.Exit(1)
+	}
+	w := bufio.NewWriter(out)
+	enc := gob.NewEncoder(w)
+	enc.Encode(settings)
+	w.Flush()
+	out.Close()
+}
+
+// ReadSettings reads settings from a file
+func ReadSettings(filename string) Settings {
+	var b Settings
+
+	in, err2 := os.Open(filename)
+	if err2 != nil {
+		fmt.Printf("File read error: %v\n", err2)
+		os.Exit(1)
+	}
+	r := bufio.NewReader(in)
+	dec := gob.NewDecoder(r)
+	dec.Decode(&b)
+    return b
 }
 
 func init() {
@@ -41,6 +91,6 @@ func init() {
 	// is called directly, e.g.:
 	// initCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 
-    // create new sqlite db
-    os.Create("oribyte.db")
+	// create new sqlite db
+	//  os.Create("oribyte.db")
 }
